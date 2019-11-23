cc04e6e3b1fe88e6115013108a7d78ce1be6b9bb
Anatolii Shpylka <shpylka.an@gmail.com>
2019-11-23 19:35:20 +0200

    set global prefix 'api' for all routes

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
